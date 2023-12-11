2bcf3feb682fc7bb593df6cd7a869b61e82b3ed7
Rishi Prabhat <rishiprabhatnarayana.sannala@sjsu.edu>
2023-12-11 04:52:45 -0800

    readme updated

--- README.md
+++ README.md
@@ -1,24 +1,46 @@
-# Project Name
+# Adaptive Stock Trading Agent
+
 ## Project Summary
-<!-- Around 200 Words -->
-<!-- Cover (1) What problem you are solving, (2) Who will use this RL module and be happy with the learning, and (3) a brief description of the results -->
+This project develops a reinforcement learning (RL) agent that harnesses Proximal Policy Optimization (PPO) to optimize trading strategies in a simulated stock market. The agent analyzes historical stock data to execute buy, sell, or hold decisions aimed at maximizing portfolio value. It balances exploration of market behavior with the exploitation of known profitable trends, enhancing its net worth and managing risks and costs.
+
+The RL agent's performance shows promise, evidenced by a general uptick in simulated portfolio value through training. Nonetheless, variability in its decision-making and periodic downturns in profitability highlight the complex nature of financial markets and the need for sophisticated, adaptive algorithms. These insights underscore the potential and challenges of applying RL to stock trading, with the goal of consistent strategy enhancement.
 
 ## State Space
-<!-- See the Cart Pole Env example https://gymnasium.farama.org/environments/classic_control/cart_pole/ -->
+The state space includes:
+
+- Historical stock prices (Open, High, Low, Close) over the last five trading days.
+- Trading volume for the same period.
+- The trader's account balance and portfolio composition.
+- Current stock holdings and the associated average cost basis.
+- The total value of stocks sold and the accumulated sales revenue.
 
 ## Action Space
-<!-- See the Cart Pole Env example https://gymnasium.farama.org/environments/classic_control/cart_pole/ -->
+Actions within this environment are:
+
+- **Trade Type**: Buy, sell, or hold.
+- **Trade Quantity**: Percentage of the portfolio balance to invest or percentage of held shares to liquidate.
 
 ## Rewards
-<!-- See the Cart Pole Env example https://gymnasium.farama.org/environments/classic_control/cart_pole/ -->
+The agent is rewarded based on the increase or decrease in portfolio value after each action. This incentivizes maximization of returns and minimization of losses. Penalties for high transaction volumes can be included to discourage overtrading.
 
-## RL Algorithm 
+## RL Algorithm
+Proximal Policy Optimization (PPO) is employed, a policy gradient algorithm suitable for the stochastic nature of stock markets.
 
-## Starting State [if applicable]
-<!-- See the Cart Pole Env example https://gymnasium.farama.org/environments/classic_control/cart_pole/ -->
+## Starting State
+Each training episode starts with:
 
-## Episode End [if applicable]
-<!-- See the Cart Pole Env example https://gymnasium.farama.org/environments/classic_control/cart_pole/ -->
+- A set portfolio balance.
+- No stock holdings.
+- The initial segment of historical market data.
+
+## Episode End
+An episode ends if:
+
+- The agent's net worth falls below a critical threshold.
+- The end of the simulated trading period is reached.
+- A pre-set number of time steps elapses.
 
 ## Results
+The RL model has shown the ability to progressively increase the agent's net worth. However, high variance in rewards and a slight dip in performance towards the end of training suggest potential overfitting. Continuous adaptation is necessary for developing a robust and generalizable strategy.
 
+![Learning Curve](images/Figure_1.png)
---
+++ intro-custom-env-and-ray/stock_trading_env.py
@@ -0,0 +1,189 @@
+import random
+import json
+import gymnasium as gym
+from gymnasium import spaces
+import pandas as pd
+import numpy as np
+
+
+class StockTradingEnvironment(gym.Env):
+    """A stock trading environment for OpenAI gym"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self,
+                df,
+                MAX_ACCOUNT_BALANCE,
+                MAX_NUM_SHARES,
+                MAX_SHARE_PRICE,
+                MAX_OPEN_POSITION,
+                MAX_STEPS,
+                INITIAL_ACCOUNT_BALANCE):
+        super(StockTradingEnvironment, self).__init__()
+
+        self.df = df
+        self.MAX_ACCOUNT_BALANCE = MAX_ACCOUNT_BALANCE
+        self.MAX_NUM_SHARES = MAX_NUM_SHARES
+        self.MAX_SHARE_PRICE = MAX_SHARE_PRICE
+        self.MAX_OPEN_POSITION = MAX_OPEN_POSITION
+        self.MAX_STEPS = MAX_STEPS
+        self.INITIAL_ACCOUNT_BALANCE = INITIAL_ACCOUNT_BALANCE
+        self.reward_range = (0, MAX_ACCOUNT_BALANCE)
+
+        # Actions of the format Buy x%, Sell x%, Hold, etc.
+        self.action_space = spaces.Box(
+            low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
+
+        # Prices contains the OHCL values for the last five prices
+        self.observation_space = spaces.Box(
+            low=0, high=1, shape=(6 * 6,), dtype=np.float16)
+    
+    def reset(self, *, seed=None, options=None):
+        # Reset the state of the environment to an initial state
+        self.balance = self.INITIAL_ACCOUNT_BALANCE
+        self.net_worth = self.INITIAL_ACCOUNT_BALANCE
+        self.max_net_worth = self.INITIAL_ACCOUNT_BALANCE
+        self.shares_held = 0
+        self.cost_basis = 0
+        self.total_shares_sold = 0
+        self.total_sales_value = 0
+
+        # Set the current step to a random point within the data frame
+        self.current_step = random.randint(
+            0, len(self.df.loc[:, 'Open'].values) - 6)
+
+        info = {}
+        return self._next_observation(), info
+
+    def _next_observation(self):
+        # Get the stock data points for the last 5 days and scale to between 0-1
+        frame = np.array([
+            self.df.loc[self.current_step: self.current_step +
+                        5, 'Open'].values / self.MAX_SHARE_PRICE,
+            self.df.loc[self.current_step: self.current_step +
+                        5, 'High'].values / self.MAX_SHARE_PRICE,
+            self.df.loc[self.current_step: self.current_step +
+                        5, 'Low'].values / self.MAX_SHARE_PRICE,
+            self.df.loc[self.current_step: self.current_step +
+                        5, 'Close'].values / self.MAX_SHARE_PRICE,
+            self.df.loc[self.current_step: self.current_step +
+                        5, 'Volume'].values / self.MAX_NUM_SHARES,
+        ])
+        
+        # Append additional data and scale each value to between 0-1
+        obs = np.append(frame, [[
+            self.balance / self.MAX_ACCOUNT_BALANCE,
+            self.max_net_worth / self.MAX_ACCOUNT_BALANCE,
+            self.shares_held / self.MAX_NUM_SHARES,
+            self.cost_basis / self.MAX_SHARE_PRICE,
+            self.total_shares_sold / self.MAX_NUM_SHARES,
+            self.total_sales_value / (self.MAX_NUM_SHARES * self.MAX_SHARE_PRICE),
+        ]], axis=0)
+
+        return obs.flatten()
+
+    def _take_action(self, action):
+        # Set the current price to a random price within the time step
+        current_price = random.uniform(
+            self.df.loc[self.current_step, "Open"], self.df.loc[self.current_step, "Close"])
+
+        action_type = action[0]
+        amount = action[1]
+
+        if action_type < 1:
+            # Buy amount % of balance in shares
+            total_possible = int(self.balance / current_price)
+            shares_bought = int(total_possible * amount)
+            prev_cost = self.cost_basis * self.shares_held
+            additional_cost = shares_bought * current_price
+
+            self.balance -= additional_cost
+            self.cost_basis = (
+                prev_cost + additional_cost) / (self.shares_held + shares_bought)
+            self.shares_held += shares_bought
+
+        elif action_type < 2:
+            # Sell amount % of shares held
+            shares_sold = int(self.shares_held * amount)
+            self.balance += shares_sold * current_price
+            self.shares_held -= shares_sold
+            self.total_shares_sold += shares_sold
+            self.total_sales_value += shares_sold * current_price
+
+        self.net_worth = self.balance + self.shares_held * current_price
+
+        if self.net_worth > self.max_net_worth:
+            self.max_net_worth = self.net_worth
+
+        if self.shares_held == 0:
+            self.cost_basis = 0
+
+    def step(self, action):
+        # Execute one time step within the environment
+        self._take_action(action)
+
+        self.current_step += 1
+
+        if self.current_step > len(self.df.loc[:, 'Open'].values) - 6:
+            self.current_step = 0
+
+        delay_modifier = (self.current_step / self.MAX_STEPS)
+        
+        reward = self.balance * delay_modifier
+
+        if self.net_worth <= 0:
+            done = False
+        else:
+            done = True
+
+        obs = self._next_observation()
+        
+        return obs, reward, done, False, {}
+
+    def render(self, mode='human', close=False):
+        # Render the environment to the screen
+        profit = self.net_worth - self.INITIAL_ACCOUNT_BALANCE
+
+        print(f'Step: {self.current_step}')
+        print(f'Balance: {self.balance}')
+        print(
+            f'Shares held: {self.shares_held} (Total sold: {self.total_shares_sold})')
+        print(
+            f'Avg cost for held shares: {self.cost_basis} (Total sales value: {self.total_sales_value})')
+        print(
+            f'Net worth: {self.net_worth} (Max net worth: {self.max_net_worth})')
+        print(f'Profit: {profit}')
+
+if __name__ == "__main__":
+
+    df = pd.read_csv("../data/AAPL.csv", index_col=[0])
+    MAX_ACCOUNT_BALANCE=2147483647
+    MAX_NUM_SHARES=2147483647
+    MAX_SHARE_PRICE=5000
+    MAX_OPEN_POSITION=5
+    MAX_STEPS=20000
+    INITIAL_ACCOUNT_BALANCE=10000
+
+    env = StockTradingEnvironment(df,
+                                  MAX_ACCOUNT_BALANCE, 
+                                  MAX_NUM_SHARES, 
+                                  MAX_SHARE_PRICE, 
+                                  MAX_OPEN_POSITION, 
+                                  MAX_STEPS, 
+                                  INITIAL_ACCOUNT_BALANCE)
+
+    env.reset()
+
+    total_reward = 0
+    done = False
+
+    for episode in range(10000):
+        action = env.action_space.sample()
+        new_obs, reward, done, _ = env.step(action)
+        
+        total_reward += reward
+
+        if episode % 1000 == 0:
+            env.render()
+        
+        if done:
+            break
